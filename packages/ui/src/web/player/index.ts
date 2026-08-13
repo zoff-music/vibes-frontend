aaeb9999a828dcb27610c53b56ce5522abbbb092
Kasper Rynning-Tønnesen <github@bundle.email>
2026-08-13 17:47:16 +0200

    fix: preserve provider autoplay unlock

--- packages/ui/src/web/player/index.ts
+++ packages/ui/src/web/player/index.ts
@@ -1,6 +1,7 @@
 export * from './AuthOverlay';
 export * from './ClickToPlayOverlay';
 export * from './PlayerControls';
+export * from './playbackGesture';
 export * from './SoundCloudPlayer';
 export * from './SpotifyPlayer';
 export * from './VideoPlayer';
